d668349132dcfc31135d68d47ff9f0e29f45313e
RafaelCesario <rafaelcesario@gmail.com>
2022-08-07 21:00:49 -0300

    primeiro commit

---
+++ src/model/Banco.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.List;
+
+public class Banco {
+
+	private String nome;
+	private List<Conta> contas;
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,25 @@
+package principal;
+
+import model.Cliente;
+import model.Conta;
+import model.ContaCorrente;
+import model.ContaPoupanca;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Cliente rafael = new Cliente();
+		rafael.setNome("Rafael");		
+		
+		Conta cc = new ContaCorrente(rafael);
+		Conta poupanca = new ContaPoupanca(rafael);
+		
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
